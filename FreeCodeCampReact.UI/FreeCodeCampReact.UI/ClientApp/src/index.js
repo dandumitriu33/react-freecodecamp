c613bd968879a342067be832102b0f79e43650bb
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-03 12:43:17 +0300

    Add first exercise - JSX, ReactDOM

--- FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/index.js
+++ FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/index.js
@@ -1,18 +1,39 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// WARNING - LEAVE THIN ON AFTER EXERCISES
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+//import 'bootstrap/dist/css/bootstrap.css';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import { BrowserRouter } from 'react-router-dom';
+//import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+//const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+//const rootElement = document.getElementById('root');
 
-registerServiceWorker();
 
+//ReactDOM.render(
+//  <BrowserRouter basename={baseUrl}>
+//    <App />
+//  </BrowserRouter>,
+//  rootElement);
+
+//registerServiceWorker();
+
+
+
+
+
+// Exercise 1 - JSX
+import React from "react";
+import ReactDom from "react-dom";
+
+//ReactDom.render(<div><h1>Hello</h1><p>This is a paragraph.</p></div>, document.getElementById("root"));
+ReactDom.render(
+    <div>
+        <ul>
+            <li>Jim</li>
+            <li>Bob</li>
+            <li>Ana</li>
+        </ul>
+    </div>,
+    document.getElementById("root"));
