2ef1504881fadf1f6130eed195c52b2d540e1a73
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-03 14:57:49 +0300

    Add Functional Components exercise

--- FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/index.js
+++ FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/index.js
@@ -24,16 +24,47 @@
 
 
 // Exercise 1 - JSX
-import React from "react";
-import ReactDom from "react-dom";
+//import React from "react";
+//import ReactDom from "react-dom";
 
 //ReactDom.render(<div><h1>Hello</h1><p>This is a paragraph.</p></div>, document.getElementById("root"));
-ReactDom.render(
-    <div>
-        <ul>
-            <li>Jim</li>
-            <li>Bob</li>
-            <li>Ana</li>
-        </ul>
-    </div>,
+
+//function MyApp() {
+//    return (
+//        <div>
+//          <p>An unordered list</p>
+//          <ul>
+//            <li>Jim</li>
+//            <li>Bob</li>
+//            <li>Ana</li>
+//          </ul>
+//        </div>
+//    )
+//}
+
+//ReactDom.render(
+//    <MyApp />,
+//    document.getElementById("root"));
+
+// Exercise 2 - Functional components
+import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Vacations() {
+    return (
+        <div class="container">
+            <h1>Bob</h1>
+            <p>These are the favorite vacation spots:</p>
+            <ol class="list-group">
+                <li class="list-group-item">Maui</li>
+                <li class="list-group-item">Bali</li>
+                <li class="list-group-item">Hawaii</li>
+            </ol>
+            </div>
+        )
+}
+
+ReactDOM.render(
+    <Vacations />,
     document.getElementById("root"));
