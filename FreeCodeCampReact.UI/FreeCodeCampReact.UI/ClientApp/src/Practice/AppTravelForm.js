a9ba2374a5661ba4d741103e58ae510ad1022c40
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-05 21:56:30 +0300

    Add separation - container/component architecture

--- FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/Practice/AppTravelForm.js
+++ FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/Practice/AppTravelForm.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from "react";
+import AppTravelFormComponent from "./AppTravelFormComponent";
 
 class AppTravelForm extends Component {
     constructor() {
@@ -56,109 +57,12 @@ class AppTravelForm extends Component {
 
     render() {
         return (
-            <main>
-                <form>
-                    <input
-                        name="firstName"
-                        value={this.state.firstName}
-                        onChange={this.handleChange}
-                        placeholder="First Name"
-                    />
-                    <br />
-
-                    <input
-                        name="lastName"
-                        value={this.state.lastName}
-                        onChange={this.handleChange}
-                        placeholder="Last Name"
-                    />
-                    <br />
-
-                    <input
-                        name="age"
-                        value={this.state.age}
-                        onChange={this.handleChange}
-                        placeholder="Age"
-                    />
-                    <br />
-                    <label>
-                        <input
-                            type="radio"
-                            name="gender"
-                            value="male"
-                            checked={this.state.gender === "male"}
-                            onChange={this.handleChange}
-                        /> Male
-                    </label>
-                    <br />
-                    <label>
-                        <input
-                            type="radio"
-                            name="gender"
-                            value="female"
-                            checked={this.state.gender === "female"}
-                            onChange={this.handleChange}
-                        /> Female
-                    </label>
-
-                    <br />
-                    <select
-                        name="destination"
-                        value={this.state.destination}
-                        onChange={this.handleChange}
-                    >
-                        <option value="">-- Please select a destination --</option>
-                        <option value="germany">Germany</option>
-                        <option value="norway">Norway</option>
-                        <option value="north pole">North Pole</option>
-                        <option value="south pole">South Pole</option>
-                        </select>
-
-                    <br />
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="isVegan"
-                            onChange={this.handleChange}
-                            checked={this.state.isVegan}
-                            /> vegan
-                    </label>
-                    <br />
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="isKosher"
-                            onChange={this.handleChange}
-                            checked={this.state.isKosher}
-                        /> kosher
-                    </label>
-                    <br />
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="isLactoseFree"
-                            onChange={this.handleChange}
-                            checked={this.state.isLactoseFree}
-                        /> lactose free
-                    </label>
-
-
-                    <br />
-                    <button>Submit</button>
-                </form>
-                <hr />
-                <h3>Entered information:</h3>
-                <p>Your name: {this.state.firstName} {this.state.lastName}</p>
-                <p>Your age: {this.state.age}</p>
-                <p>Your gender: {this.state.gender}</p>
-                <p>Your destination: {this.state.destination}</p>
-                <p>Your dietary restrictions:
-                    <p>{this.state.isVegan ? "Vegan" : ""}</p>
-                    <p>{this.state.isKosher ? "Kosher" : ""}</p>
-                    <p>{this.state.isLactoseFree ? "Lactose Free" : ""}</p>
-                </p>
-            </main>
+            <AppTravelFormComponent
+                handleChange={this.handleChange}
+                data={this.state}
+            />
         )
+        
     }
 }
 
---
+++ FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/Practice/AppTravelFormComponent.js
@@ -0,0 +1,110 @@
+﻿import React from "react";
+
+function AppTravelFormComponent(props) {
+    return (
+        <main>
+            <form>
+                <input
+                    name="firstName"
+                    value={props.data.firstName}
+                    onChange={props.handleChange}
+                    placeholder="First Name"
+                />
+                <br />
+
+                <input
+                    name="lastName"
+                    value={props.data.lastName}
+                    onChange={props.handleChange}
+                    placeholder="Last Name"
+                />
+                <br />
+
+                <input
+                    name="age"
+                    value={props.data.age}
+                    onChange={props.handleChange}
+                    placeholder="Age"
+                />
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={props.data.gender === "male"}
+                        onChange={props.handleChange}
+                    /> Male
+                        </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={props.data.gender === "female"}
+                        onChange={props.handleChange}
+                    /> Female
+                        </label>
+
+                <br />
+                <select
+                    name="destination"
+                    value={props.data.destination}
+                    onChange={props.handleChange}
+                >
+                    <option value="">-- Please select a destination --</option>
+                    <option value="germany">Germany</option>
+                    <option value="norway">Norway</option>
+                    <option value="north pole">North Pole</option>
+                    <option value="south pole">South Pole</option>
+                </select>
+
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        name="isVegan"
+                        onChange={props.handleChange}
+                        checked={props.data.isVegan}
+                    /> vegan
+                        </label>
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        name="isKosher"
+                        onChange={props.handleChange}
+                        checked={props.data.isKosher}
+                    /> kosher
+                        </label>
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        name="isLactoseFree"
+                        onChange={props.handleChange}
+                        checked={props.data.isLactoseFree}
+                    /> lactose free
+                        </label>
+
+
+                <br />
+                <button>Submit</button>
+            </form>
+            <hr />
+            <h3>Entered information:</h3>
+            <p>Your name: {props.data.firstName} {props.data.lastName}</p>
+            <p>Your age: {props.data.age}</p>
+            <p>Your gender: {props.data.gender}</p>
+            <p>Your destination: {props.data.destination}</p>
+            <div>Your dietary restrictions:
+                <p>{props.data.isVegan ? "Vegan" : ""}</p>
+                <p>{props.data.isKosher ? "Kosher" : ""}</p>
+                <p>{props.data.isLactoseFree ? "Lactose Free" : ""}</p>
+            </div>
+        </main>
+    )
+}
+
+export default AppTravelFormComponent;
