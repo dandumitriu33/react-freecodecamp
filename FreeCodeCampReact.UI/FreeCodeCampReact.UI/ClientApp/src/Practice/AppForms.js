9351a6ac24cd1663f9662d3c03be9e6ed4a2dd99
Dan Dumitriu <dandumitriu33@gmail.com>
2020-08-05 20:17:08 +0300

    Add forms part 2

--- FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/Practice/AppForms.js
+++ FreeCodeCampReact.UI/FreeCodeCampReact.UI/ClientApp/src/Practice/AppForms.js
@@ -5,37 +5,103 @@ class AppForms extends Component {
         super();
         this.state = {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            isFriendly: false,
+            gender: "",
+            favColor: "blue"
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        //const { name, value } = event.target;
+        //const { name, value, type, checked } = event.target;
         let name = event.target.name;
         let value = event.target.value;
-        this.setState({
-            [name]: value
-        })
+        let type = event.target.type;
+        let checked = event.target.checked;
+        if (type === "checkbox") {
+            this.setState({
+                [name]: checked
+            })
+        }
+        else {
+            this.setState({
+                [name]: value
+            })
+        }
+        
     }
 
+    // either use a form onSubmit action or on the button onClick to do stuff with the form data
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input
                     type="text"
                     value={this.state.firstName}
                     name="firstName"
                     placeholder="First Name"
-                    onChange={this.handleChange} />
+                    onChange={this.handleChange}
+                />
                 <br />
                 <input
                     type="text"
                     value={this.state.lastName}
                     name="lastName"
                     placeholder="Last Name"
-                    onChange={this.handleChange} />
+                    onChange={this.handleChange}
+                />
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
+                <br />
+                <textarea
+                    value={"some default value"}
+                    onChange={this.handleChange}
+                />
+                <br />
+                <label>
+                    <input
+                        type="checkbox"
+                        name="isFriendly"
+                        checked={this.state.isFriendly}
+                        onChange={this.handleChange}
+                    />Is friendly?
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={this.state.gender === "male"}
+                        onChange={this.handleChange}
+                    />Male
+                </label>
+                <br />
+                <label>
+                    <input
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={this.state.gender === "female"}
+                        onChange={this.handleChange}
+                    />Female
+                </label>
+                <h4>Selected gender: {this.state.gender}</h4>
+
+                <label>Favorite color: </label>
+                <select
+                    value={this.state.favColor}
+                    onChange={this.handleChange}
+                    name="favColor"
+                >
+                    <option value="red">Red</option>
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="orange">Orange</option>
+                </select>
+                <h4>Your favorite color is {this.state.favColor}.</h4>
+                <button>Submit</button>
             </form>
             )
     }
